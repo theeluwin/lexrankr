a6fd9114ac62e2870ef9281f0190b594c7d1cb55
theeluwin <theeluwin@gmail.com>
2018-05-20 18:24:09 +0900

    jpype1(-3) should be installed properly rather than just pip install, in Windows

--- setup.py
+++ setup.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import os
 import sys
+import warnings
 
 from setuptools import find_packages, setup
 from setuptools.command.test import test
@@ -16,14 +18,18 @@ requirements = [
     'gensim',
     'sklearn',
 ]
-if sys.version_info < (3, ):
-    requirements.append('jpype1')
+
+if os.name == 'nt':
+    warnings.warn("See http://konlpy.org/en/v0.4.4/install/#id2 to properly install konlpy.", RuntimeWarning)
 else:
-    requirements.append('jpype1-py3')
+    if sys.version_info < (3, ):
+        requirements.append('jpype1')
+    else:
+        requirements.append('jpype1-py3')
 
 setup(
     name='lexrankr',
-    version='0.1.5',
+    version='0.1.6',
     license='MIT',
     author='Jamie Seol',
     author_email='theeluwin@gmail.com',
